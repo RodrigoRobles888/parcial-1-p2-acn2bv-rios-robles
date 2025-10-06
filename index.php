a5e334353a1b99ce130cc644ef89fba389cfe385
MayraS777 <mayrasofiaa3664@gmail.com>
2025-10-05 21:26:16 -0300

    mejoramos formato, simplificamos codigo

--- index.php
+++ index.php
@@ -1,50 +1,11 @@
 <?php
+require_once("./utils/data.php");
 
 $topic = isset($_GET["topic"]);
-$bmw = (object) [
-	'name' => 'BMW',
-	'url' => 'https://w7.pngwing.com/pngs/538/981/png-transparent-bmw-logo-bmw-car-logo-bmw-logo-trademark-logo-graphics.png',
-	'link' => 'https://www.bmw.com.ar/es/all-models.html'
-];
-$mercedes = (object) [
-	'name' => 'MERCEDES BENZ',
-	'url' => 'https://w7.pngwing.com/pngs/129/888/png-transparent-mercedes-benz-sprinter-car-mercedes-benz-c-class-luxury-vehicle-benz-logo-angle-trademark-triangle.png',
-	'link' => 'https://www.mercedes-benz.com.ar/passengercars/models.html?group=all&subgroup=see-all&filters='
-];
-$audi = (object) [
-	'name' => 'AUDI',
-	'url' => 'https://w7.pngwing.com/pngs/905/811/png-transparent-audi-q5-car-logo-audi-a3-audi-brand-text-vehicle-map-thumbnail.png',
-	'link' => 'https://www.audi.com.ar/es/modelos/'
-];
-$volkswagen = (object) [
-	'name' => 'VOLKSWAGEN',
-	'url' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMsxaDxFlD8rE_ah9GXtvWxxvK8sAX6WRsbQ&s',
-	'link' => 'https://www.volkswagen.com.ar/es/modelos.html'
-];
-$peugeot = (object) [
-	'name' => 'PEUGEOT',
-	'url' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO9KO5EhBdNIBdA9xgAQ971I0KTwc6dh1IoQ&s',
-	'link' => 'https://www.peugeot.com.ar/modelos/peugeot-gama.html'
-];
-$honda = (object) [
-	'name' => 'HONDA',
-	'url' => 'https://cdn.imgbin.com/24/16/1/imgbin-honda-logo-car-honda-city-honda-accord-honda-silver-honda-emblem-wAR2UeNMsA9rJw8BG1FcD6Vpj.jpg',
-	'link' => 'https://autos.honda.com.ar/'
-];
-$alfaromeo = (object) [
-	'name' => 'ALFAROMEO',
-	'url' => 'https://w7.pngwing.com/pngs/579/799/png-transparent-alfa-romeo-romeo-car-alfa-romeo-giulia-alfa-romeo-spider-alfa-romeo-emblem-trademark-logo.png',
-	'link' => 'https://www.alfaromeo.com.ar/'
-];
-$jeep = (object) [
-	'name' => 'JEEP',
-	'url' => 'https://i.pinimg.com/736x/11/ee/ec/11eeec868f528be972314d1f07ac212f.jpg',
-	'link' => 'https://www.jeep.com.ar/'
-];
 
-if ($topic == "Alta Gama"){
+if ($topic == "alta gama"){
     $marcas = [$bmw, $mercedes, $audi, $alfaromeo, $jeep];
-} else if ($topic == "Media Gama") {
+} else if ($topic == "media gama") {
     $marcas = [$volkswagen, $peugeot, $honda]; 
 } else {
     $marcas = [$bmw, $mercedes, $audi, $volkswagen, $peugeot, $honda, $alfaromeo, $jeep];
