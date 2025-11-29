b7dff2ff96fed5bcc64cd7e59d04f0019ff97863
MayraS777 <mayrasofiaa3664@gmail.com>
2025-11-29 18:53:13 -0300

    Parcial 2
    Realizamos varias modificaciones.
    Incorporamos la api.php y decidimos tomar los datos e ingresar las cargas de datos con data.json.
    Nos modifico mucho el codigo de index.php mejoramos los codigos, sacamos el body y lo dejamos fijo en index, creamos el footer independiente para que sea mas facil incorporarlo en otros sitios, mantuvimos el formulario de contacto pero en otro .php, refactorizamos muchas partes del codigo y lo mejoramos.

--- components/styleSetup.php
+++ components/styleSetup.php
@@ -15,6 +15,7 @@ if ($tema === 'oscuro') {
         background-color: <?= $bgColor ?>;
         color: <?= $textColor ?>;
     }
+
     header {
         background-color: rgba(20, 20, 20, 1);
         color: white;
@@ -22,35 +23,43 @@ if ($tema === 'oscuro') {
         font-size: 20px;
         padding: 15px;
     }
+
     header nav a {
         color: white;
         margin: 10px;
         font-weight: 600;
         text-decoration: none;
     }
+
     header nav a:hover {
-        background-color: rgba(0,0,0,0.1);
+        background-color: rgba(0, 0, 0, 0.1);
         transform: scale(1.2);
     }
-    .tituloppal, .descripcionpag {
+
+    .tituloppal,
+    .descripcionpag {
         text-align: center;
     }
+
     .container {
         display: flex;
         flex-wrap: wrap;
         justify-content: center;
     }
+
     .card {
         margin: 15px;
         border-radius: 10px;
-        box-shadow: 0 2px 5px rgba(0,0,0,0.2);
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
     }
+
     .formulario {
         padding: 5%;
         background-color: rgba(0, 0, 0, 0.1);
         margin: 4%;
         border-radius: 10px;
     }
+
     footer {
         background-color: rgba(20, 20, 20, 1);
         color: white;
